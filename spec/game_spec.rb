2ce454f498ba08ebc4e8bb49b7dfc1e8495b6a3e
Andy <andy.bell.github@gmail.com>
2016-06-07 16:42:02 +0100

    refactored controller

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,9 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
-  let(:andy){ double(:andy) }
-  let(:matt){ double(:matt, take_damage: true) }
+  subject(:game) { described_class.new(andy,matt) }
+  let(:andy){ double(:player) }
+  let(:matt){ double(:player, take_damage: true) }
 
   describe '#attack' do
     it 'one player damages the other' do
@@ -11,4 +11,17 @@ describe Game do
       game.attack(matt)
     end
   end
+
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq(andy)
+    end
+  end
+
+  describe'#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player_2).to eq(matt)
+    end
+  end
+
 end
